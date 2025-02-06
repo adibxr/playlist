4b24d6de9ae2bafead3ddc856c5bcffa00a60d03
ADITYA RAJ <107928214+adibxr@users.noreply.github.com>
2025-02-06 07:08:12 +0000

    ADI2.0

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    particlesJS("particles-js", {
+        "particles": {
+            "number": { "value": 100 },
+            "size": { "value": 3 },
+            "move": { "speed": 2 }
+        }
+    });
+
+    const volumeSlider = document.getElementById("volume-slider");
+    volumeSlider.addEventListener("input", function () {
+        let volume = volumeSlider.value / 100;
+        document.getElementById("spotify-frame").contentWindow.postMessage({ type: "setVolume", volume }, "*");
+    });
+});
